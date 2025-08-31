4bdad1ec305c56659e7aee6b0dc4c6ded1cbdc42
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-01 03:37:56 +0600

    added c0ep6

---
+++ Accelerated C++ Practical Programming by Example/c0/exercises/ep6.cpp
@@ -0,0 +1,11 @@
+//What about this one?
+#include <iostream>
+int main()
+{
+/* This is a comment that extends over several lines
+because it uses /* and */ as its starting and ending delimiters */
+std::cout << "Does this work?" << std::endl;
+return 0;
+}
+
+// this code show error becz multi line cmnt end smybol '*/' was used before actual cmnt end
