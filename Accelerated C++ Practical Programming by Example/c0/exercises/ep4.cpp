31649bdbbf0891e89fe596054b92bb815f5361bf
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-01 03:30:35 +0600

    added c0ep4

---
+++ Accelerated C++ Practical Programming by Example/c0/exercises/ep4.cpp
@@ -0,0 +1,7 @@
+//What does the following statement do?
+// 3 + 4
+
+#include <iostream>
+int main() std::cout << "Hello, world!" << std::endl;
+// not valid program -- {} function scope is missing
+
