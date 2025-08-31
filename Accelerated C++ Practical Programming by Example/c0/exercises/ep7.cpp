b517c4b2c47af7b9c870b8ea9a4414a4c2eb2cc5
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-01 03:41:57 +0600

    added c0ep7

---
+++ Accelerated C++ Practical Programming by Example/c0/exercises/ep7.cpp
@@ -0,0 +1,12 @@
+//...and this one?
+#include <iostream>
+int main()
+{
+// This is a comment that extends over several lines
+// by using // at the beginning of each line instead of using /*
+// or */ to delimit comments.
+std::cout << "Does this work?" << std::endl;
+return 0;
+}
+// yes this code works
+// output: does this work
