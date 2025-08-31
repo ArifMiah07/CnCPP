af4223e49b13f4d04758496d70c1d71d76ec1b38
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-01 03:33:13 +0600

    added c0ep5

--- Accelerated C++ Practical Programming by Example/c0/exercises/ep4.cpp
+++ Accelerated C++ Practical Programming by Example/c0/exercises/ep4.cpp
@@ -1,5 +1,6 @@
-//What does the following statement do?
-// 3 + 4
+//Is this a valid program? Why or why not?
+//#include <iostream>
+//int main() std::cout << "Hello, world!" << std::endl;
 
 #include <iostream>
 int main() std::cout << "Hello, world!" << std::endl;
---
+++ Accelerated C++ Practical Programming by Example/c0/exercises/ep5.cpp
@@ -0,0 +1,9 @@
+//Is this a valid program? Why or why not?
+//#include <iostream>
+//int main() {{{{{{ std::cout << "Hello, world!" << std::endl; }}}}}}
+
+
+#include <iostream>
+int main() {{{{{{ std::cout << "Hello, world!" << std::endl; }}}}}}
+// yes this is a valid program becz is the scope return something and no syntax error
+
