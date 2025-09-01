058295833bfbde3f7193cd86e885aa46749d5fff
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-01 22:02:32 +0600

    updt str c1

---
+++ Accelerated C++ Practical Programming by Example/c1/string_c1.cpp
@@ -0,0 +1,15 @@
+// Variables can be defined in one of three ways:
+
+#include<iostream>
+#include<string>
+
+int main()
+{
+
+    // 1
+    std::string greet = "Hello";
+    // 2
+    std::string stars(100, '*');
+    // 3
+    std::string name;
+}
