9abd1fd13651de21d2db190694ca9ad205fbe408
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-01 22:27:31 +0600

    checked c1ep3

--- Accelerated C++ Practical Programming by Example/c1/c1ep1.cpp
+++ Accelerated C++ Practical Programming by Example/c1/c1ep1.cpp
@@ -18,8 +18,25 @@ std::cout << s << std::endl; }}
 
 /*this code also valid becz syntx is correct and as ';' semi colon means end of of the statmen so a';' dosnt throw any error */
 
-//
+// exp3 -->
+// Is this program valid? If so, what does it do? If not, say why not, and rewrite it to
+//be valid.
+    {
+        std::string s = "a string";
+            {
+                std::string x = s + ", really";
+                std::cout << s << std::endl;
+                std::cout << x << std::endl; // bring that line here
+            }
+        // std::cout << x << std::endl;
+    }
 
+// the og program is not valid --> variable x was declard another block scope as 1st block scope is not able to acess other block scope it throws an error
+// after rewriting -->
+// i fixed that error by moving [std::cout << x << std::endl;] in the inner scope --> program now showing expected correct output
+
+
+//
 
 
 return 0;
