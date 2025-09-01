f49a54e131c7993f978a977ba4cce6db9905be0d
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-01 22:05:01 +0600

    checked exp1

--- Accelerated C++ Practical Programming by Example/c1/string_c1.cpp
+++ Accelerated C++ Practical Programming by Example/c1/string_c1.cpp
@@ -7,9 +7,15 @@ int main()
 {
 
     // 1
-    std::string greet = "Hello";
     // 2
     std::string stars(100, '*');
     // 3
     std::string name;
+    //1
+    std::string hello = "Hello";
+    const std::string message = hello + ", world" + "!";
+
+    return 0;
 }
+
+// exp1 this is ocde is valid
