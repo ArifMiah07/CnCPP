c86578f1b23a27cc67279e696bad176ab26941f3
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-01 22:14:16 +0600

    checked c1ep2

--- Accelerated C++ Practical Programming by Example/c1/c1ep1.cpp
+++ Accelerated C++ Practical Programming by Example/c1/c1ep1.cpp
@@ -7,6 +7,21 @@ int main()
 std::cout << s << std::endl; }
 { const std::string s = "another string";
 std::cout << s << std::endl; }
+
+
+// exp2 -->
+//What about this one? What if we change }} to };} in the third line from the end?
+{ const std::string s = "a string";
+std::cout << s << std::endl;
+{ const std::string s = "another string";
+std::cout << s << std::endl; }}
+
+/*this code also valid becz syntx is correct and as ';' semi colon means end of of the statmen so a';' dosnt throw any error */
+
+//
+
+
+
 return 0;
 }
 
