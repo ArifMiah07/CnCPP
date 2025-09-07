c525a7b49ec7bcbe63029e19ad1304b15d6d8a86
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-07 17:43:54 +0600

    Hello C++ World from VS Code and the C++ extension!

---
+++ Accelerated C++ Practical Programming by Example/c1-string/exercises/loop_cp1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+int main()
+{
+
+
+    std::cout << "Hello, World!" << std::endl;
+    int r = 0;
+    while(r <= 10){
+        std::cout << "Hello,  " << r << std::endl;
+        r++;
+    }
+
+
+    return 0;
+}
