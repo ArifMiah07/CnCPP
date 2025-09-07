195d82fb6c7615853dba0e7398e67819ca635d58
ArifMiah07 <arifmiah.me322@gmail.com>
2025-09-07 22:14:59 +0600

    updt >> rewriting hello world

--- Accelerated_Cpp/c2-loop/hello.cpp
+++ Accelerated_Cpp/c2-loop/hello.cpp
@@ -15,6 +15,47 @@ int main()
 
     // we have to rewrite this part...
 
+    // the number of black surrounding the greeding
+    const int pad = 1;
+
+    // total number of rows to write
+    const int rows = pad * 2 + 3;
+
+    // separate the output from the input
+    std::cout << std::endl;
+
+    // write rows rows of output
+    // invariant: we have written r rows so far
+    int r = 0;
+
+    // setting r to 0 makes invariant true
+    while(r != rows){
+        // we can assume that the invariant is true here
+        // writing a row of output makes the invariant false
+        std::cout << std::endl;
+        // incrementing r makes the invariants true again
+        ++r;
+    }
+    // we can conclude that the invariant is true here
+
+    //
+    const std::string::size_type cols = greeting.size() + pad * 2 + 2;
+
+    // 
+    std::string::size_type c = 0;
+    // invariant : we have written c character so far in the current row
+    while(c != cols){
+        // write one or more characters
+        // adjust the value of c to maintain the invariant
+        if(r == 0 || r == rows - 1 || c == 0 || c == cols - 1){
+            std::cout << "*";
+        }else{
+            // write on or more nonborder characters
+            /// adjust the value of c to maintain the invariant
+        }
+    }
+
+
 
     return 0;
 }
